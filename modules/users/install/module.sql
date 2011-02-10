500d925046296bf0303b24958f40c5a5a4ae7e71
Goncalo Margalho <gsky89@gmail.com>
2011-02-10 16:58:40 +0100

    added sql for Users module

---
+++ modules/users/install/module.sql
@@ -0,0 +1,1 @@
+INSERT INTO `##PREFIX##dev_menus` VALUES(null ,104,'users_manage',1,'?p=mod&mod=users',1,0, Users),(null,104,'users_groups',1,'?p=mod&mod=users&m=groups',2,0,Users),(null,104,'Users',1,'0',0,0,0);
